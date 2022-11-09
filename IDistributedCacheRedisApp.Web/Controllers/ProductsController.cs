d172d81b9d6e325f3ad0d42ff6d93fb56a739bdd
dogustuluk <dogus.tuluk@gmail.com>
2022-11-09 17:51:17 +0300

    caching complex types

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
-﻿ using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -12,21 +16,43 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache = distributedCache;
 
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
-            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            
-            _distributedCache.SetString("name", "dogus", cacheEntryOptions);
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(15);
+
+            //_distributedCache.SetString("name", "dogus", cacheEntryOptions);
+
+            Product product = new Product { Id=2, Name="Kalem2", Price=100 };
+            //json serializer
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            //await _distributedCache.SetStringAsync("product:2", jsonProduct, cacheEntryOptions);
 
 
+
+            //binary serialize
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+            _distributedCache.Set("product:3", byteProduct, cacheEntryOptions);
 
             return View();
         }
         public IActionResult Show()
         {
-            string name = _distributedCache.GetString("name"); //memory'deki datayı okuyabiliriz.
-            ViewBag.name = name;
+            //string name = _distributedCache.GetString("name"); //memory'deki datayı okuyabiliriz.
+
+            Byte[] byteProduct = _distributedCache.Get("product:3");
+
+            //string jsonProduct = _distributedCache.GetString("product:1");
+
+            //binary deserialize
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            
+            ViewBag.product = p;
             return View();
         }
         public IActionResult Remove()
